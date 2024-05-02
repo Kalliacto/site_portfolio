309176c5ce57f845e0f400c0699e4c421d53c808
Karina Boronina <Gubashina@yandex.ru>
2024-05-02 09:42:59 +0300

    перенос на реакт, создание footer,header,nav + немного main стилей

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from './components/footer';
+import Navbar from './components/navbar';
+import './styles/main.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
