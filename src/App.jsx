3ea68819a406d3c13d0f4bdb19a5ca643a92ec82
Karina Boronina <Gubashina@yandex.ru>
2024-05-04 13:58:52 +0300

    add previeward & contacts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Footer from './components/footer';
 import Navbar from './components/navbar';
+import Contacts from './pages/contacts/inadex';
 import HomePage from './pages/homePage/inadex';
 import Projects from './pages/projects/inadex';
 import './styles/main.css';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <Navbar />
             {/* <HomePage /> */}
-            <Projects />
+            {/* <Projects /> */}
+            <Contacts />
             <Footer />
         </div>
     );
--- src/pages/projects/inadex.jsx
+++ src/pages/projects/inadex.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './style.css';
-import img1 from '../../assets/images/projects/1.png';
-import img2 from '../../assets/images/projects/2.png';
-import img3 from '../../assets/images/projects/3.png';
-import img4 from '../../assets/images/projects/4.png';
-import img5 from '../../assets/images/projects/5.png';
-import img6 from '../../assets/images/projects/6.png';
+import projectsCards from '../../assets/data/projectsCard.json';
+import PreviewCard from '../../components/previewCard';
 
 const Projects = (props) => {
     return (
@@ -13,38 +9,11 @@ const Projects = (props) => {
             <div className='container'>
                 <h2 className='title-1'>Projects</h2>
                 <ul className='projects'>
-                    <li className='project'>
-                        <a href='./project-page.html'>
-                            <img src={img1} alt='Project img' className='project__img' />
-                            <h3 className='project__title'>Video service of popular videos You Tube</h3>
-                        </a>
-                    </li>
-                    <li className='project'>
-                        <a href='./project-page.html'>
-                            <img src={img2} alt='Project img' className='project__img' />
-                            <h3 className='project__title'>Online Lingerie Store</h3>
-                        </a>
-                    </li>
-                    <li className='project'>
-                        <a href='./project-page.html'>
-                            <img src={img3} alt='Project img' className='project__img' />
-                            <h3 className='project__title'>Online Bird Goods Store</h3>
-                        </a>
-                    </li>
-                    <li className='project'>
-                        <img src={img4} alt='Project img' className='project__img' />
-                        <h3 className='project__title'>
-                            The website page dedicated to the series "Avatar: The Legend of Aang"
-                        </h3>
-                    </li>
-                    <li className='project'>
-                        <img src={img5} alt='Project img' className='project__img' />
-                        <h3 className='project__title'>Landing NTF Marketplace</h3>
-                    </li>
-                    <li className='project'>
-                        <img src={img6} alt='Project img' className='project__img' />
-                        <h3 className='project__title'>Weather website</h3>
-                    </li>
+                    {projectsCards.map((card) => (
+                        <li key={card.id} className='project'>
+                            <PreviewCard card={card} />
+                        </li>
+                    ))}
                 </ul>
             </div>
         </main>
