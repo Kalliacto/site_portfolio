53504cb60dd1888f128b4bf458b1c4520749b6aa
Karina Boronina <Gubashina@yandex.ru>
2024-05-07 15:18:13 +0300

    -1 TODO

--- src/components/previewCard/index.jsx
+++ src/components/previewCard/index.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 
 const PreviewCard = ({ card }) => {
     return (
-        <article className='project__wrap'>
-            <Link to={`/project/${card.id}`} className='project__card'>
-                <img src={projectsMiniImg[card.miniImg]} alt={card.title} className='project__img' />
-                <h3 className='project__title'>{card.title}</h3>
-            </Link>
-        </article>
+        // <article className='project__wrap'>
+        <Link to={`/project/${card.id}`} className='project__card'>
+            <img src={projectsMiniImg[card.miniImg]} alt={card.title} className='project__img' />
+            <h3 className='project__title'>{card.title}</h3>
+        </Link>
+        // </article>
     );
 };
 
