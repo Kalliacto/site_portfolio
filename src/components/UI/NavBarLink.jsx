63ac3cca901ba13a09fc414b1ddb52193f997209
Karina Boronina <Gubashina@yandex.ru>
2024-05-08 09:24:21 +0300

    ScrollToTop add

--- src/components/UI/NavBarLink.jsx
+++ src/components/UI/NavBarLink.jsx
@@ -3,8 +3,11 @@ import './style.css';
 import { NavLink } from 'react-router-dom';
 
 const NavBarLink = ({ path, title }) => {
+    const simpleLink = 'nav-list__link';
+    const activeLink = 'nav-list__link--active';
+
     return (
-        <NavLink to={path} className={({ isActive }) => 'nav-list__link' + (isActive ? ' nav-list__link--active' : '')}>
+        <NavLink to={path} className={({ isActive }) => (isActive ? activeLink : simpleLink)}>
             {title}
         </NavLink>
     );
